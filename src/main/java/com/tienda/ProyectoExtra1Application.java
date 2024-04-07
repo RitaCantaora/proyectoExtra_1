7f9598f62470bd8e62e538c5eb6d401346fd4b93
mdegs02 <mdegs02@educastur.es>
2024-04-07 22:06:04 +0200

    rutas

---
+++ src/main/java/com/tienda/ProyectoExtra1Application.java
@@ -0,0 +1,13 @@
+package com.tienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoExtra1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoExtra1Application.class, args);
+	}
+
+}
